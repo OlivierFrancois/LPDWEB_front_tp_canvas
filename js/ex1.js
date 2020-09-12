061b4f2b26e69ae734e14072971825458330257b
OlivierFrancois <olivier.francois09@gmail.com>
2020-09-12 14:34:06 +0200

    draw random circle when user click

--- js/ex1.js
+++ js/ex1.js
@@ -8,13 +8,31 @@ let globalY = document.getElementById('global-y');
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-console.log(canvas);
-
+// Position de la souris
 canvas.addEventListener('mousemove', (e) => {
 	globalX.innerText = e.clientX;
 	globalY.innerText = e.clientY;
 
 	let offset = [canvas.offsetLeft, canvas.offsetTop];
 	localX.innerText = e.clientX - offset[0];
-	localY.innerText = e.clientY - offset[1];;
+	localY.innerText = e.clientY - offset[1];
+})
+
+// Dessin de cercles
+canvas.addEventListener('click', (e) => {
+	const max = 100;
+	randomRadius = Math.floor(Math.random() * Math.floor(max));
+	randomColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+
+	console.log(randomColor);
+	ctx.beginPath();
+	ctx.fillStyle = randomColor;
+
+	let x = e.clientX - canvas.offsetLeft;
+	let y = e.clientY - canvas.offsetTop;
+	let angleFrom = 0;
+	let angleTo = 2 * Math.PI;
+
+	ctx.arc(x, y, randomRadius, angleFrom, angleTo, false);
+	ctx.fill();
 })
