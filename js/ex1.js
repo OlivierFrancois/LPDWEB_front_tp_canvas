04a6aac29cf2af5ceb37d3e702e84e9fec9c0fda
OlivierFrancois <olivier.francois09@gmail.com>
2020-09-12 14:20:03 +0200

    get local and global position of mouse in canvas

--- js/ex1.js
+++ js/ex1.js
@@ -1,2 +1,20 @@
-const canvas = document.getElementById('canvas');
+// Création d'un p pour stocker les informations du curseur
+let localX = document.getElementById('local-x');
+let localY = document.getElementById('local-y');
+
+let globalX = document.getElementById('global-x');
+let globalY = document.getElementById('global-y');
+
+const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+
+console.log(canvas);
+
+canvas.addEventListener('mousemove', (e) => {
+	globalX.innerText = e.clientX;
+	globalY.innerText = e.clientY;
+
+	let offset = [canvas.offsetLeft, canvas.offsetTop];
+	localX.innerText = e.clientX - offset[0];
+	localY.innerText = e.clientY - offset[1];;
+})
