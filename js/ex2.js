de46795064aa9c056770b9dbda0283af541dd638
Olivier Francois <olivier.francois09@gmail.com>
2020-09-15 09:13:51 +0200

    Dessin du damier

--- js/ex2.js
+++ js/ex2.js
@@ -1,2 +1,31 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+
+// Création du damier
+function drawCheckerBoard (color, checkerBoardSize) {
+    ctx.fillStyle = color;
+    const wCell = width / checkerBoardSize;
+    const hCell = height / checkerBoardSize;
+
+
+    let fillCell = true; // Booléen servant à définir si la cellule du i, j du damier doit être remplie ou non
+    for (let i = 0; i < Math.floor(width/checkerBoardSize); i++) {
+        for (let j = 0; j < Math.floor(height/checkerBoardSize); j++) {
+            // Si la cellule est à remplir, on remplit puis on toggle fillCell
+            if (fillCell) {
+                ctx.fillRect(i*wCell, j*hCell, wCell, hCell);
+                fillCell = !fillCell;
+            }
+            else {
+                fillCell = !fillCell;
+            }
+        }
+        // Sans cette ligne, on ne dessine que des rangées de couleur
+        fillCell = !fillCell;
+    }
+}
+
+drawCheckerBoard('#008B8B', 10);
+
